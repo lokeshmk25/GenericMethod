3a6a6c736475e14ecc4089c54464e03e19f15bc7
lokeshmk25 <lokeshmk2211@gmail.com>
2021-08-24 21:46:03 +0530

    Added:Maximum of three float numbers

--- src/com/bridgelabz/MaxNumber.java
+++ src/com/bridgelabz/MaxNumber.java
@@ -1,7 +1,7 @@
 package com.bridgelabz;
 
 public class MaxNumber {
-    public static int maximum() {
+    public static int maximumint() {
         Integer obj1 = new Integer("50");
         Integer obj2 = new Integer("30");
         Integer obj3 = new Integer("10");
@@ -12,11 +12,26 @@ public class MaxNumber {
             max=obj2;
         if(obj3.compareTo(max)>0)
             max=obj3;
-        System.out.println("Maximum of three number is :" +max);
+        System.out.println("Maximum of three intnumber is :" +max);
         return max;
+    }
+    public static int maximumfloat() {
+        Float obj1 = new Float("5.5");
+        Float obj2 = new Float("3.8");
+        Float obj3 = new Float("10.1");
+
+        float max=obj1;
+
+        if(obj2.compareTo(max)>0)
+            max=obj2;
+        if(obj3.compareTo(max)>0)
+            max=obj3;
+        System.out.println("Maximum of three floatnumber is :" +max);
+        return (int) max;
     }
 
     public static void main(String[] args) {
-        maximum();
+        maximumint();
+        maximumfloat();
     }
     }
