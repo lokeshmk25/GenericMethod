b4a8a2bf40963cad42bbb26e61f2708e95e8d57d
lokeshmk25 <lokeshmk2211@gmail.com>
2021-08-24 22:17:35 +0530

    Added : Maximum of 3 String

--- src/com/bridgelabz/MaxNumber.java
+++ src/com/bridgelabz/MaxNumber.java
@@ -29,9 +29,23 @@ public class MaxNumber {
         System.out.println("Maximum of three floatnumber is :" +max);
         return (int) max;
     }
+    public static void maximumString() {
+        String obj1 = new String("I");
+        String obj2 = new String("am");
+        String obj3 = new String("BATMAN");
+
+        String max=obj1;
+
+        if(obj2.compareTo(max)>0)
+            max=obj2;
+        if(obj3.compareTo(max)>0)
+            max=obj3;
+        System.out.println("Maximum of three Stringnumber is :" +max);
+    }
 
     public static void main(String[] args) {
         maximumint();
         maximumfloat();
+        maximumString();
     }
     }
