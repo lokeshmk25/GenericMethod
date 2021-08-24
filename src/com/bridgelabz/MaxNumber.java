90e16a4daee32819cf26fe26f60d643386d0c259
lokeshmk25 <lokeshmk2211@gmail.com>
2021-08-24 21:39:17 +0530

    Added: maximum of 3 integers

--- src/com/bridgelabz/MaxNumber.java
+++ src/com/bridgelabz/MaxNumber.java
@@ -1,7 +1,22 @@
 package com.bridgelabz;
 
 public class MaxNumber {
-    public static void main(String[] args) {
-        System.out.println("welcome to max number method");
+    public static int maximum() {
+        Integer obj1 = new Integer("50");
+        Integer obj2 = new Integer("30");
+        Integer obj3 = new Integer("10");
+
+        int max=obj1;
+
+        if(obj2.compareTo(max)>0)
+            max=obj2;
+        if(obj3.compareTo(max)>0)
+            max=obj3;
+        System.out.println("Maximum of three number is :" +max);
+        return max;
     }
-}
+
+    public static void main(String[] args) {
+        maximum();
+    }
+    }
